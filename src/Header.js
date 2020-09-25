0ca40335795fb2bd311c7ba0699690ab5f864591
nohsangwoo <nsgr12@naver.com>
2020-09-25 22:31:22 +0900

    add to material icon

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/Subscriptions";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+
 function Header() {
   return (
     <div className="header">
-      <div className="header__left"></div>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
-        alt="facebook logo"
-      />
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+          alt="facebook logo"
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
 
-      <div className="header__milddle"></div>
+      <div className="header__milddle">
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
 
       <div className="header__right"></div>
     </div>
