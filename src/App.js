d9af088e9b4b278a0ed92d8b61bac1d2dac93bff
nohsangwoo <nsgr12@naver.com>
2020-09-27 21:35:06 +0900

    finish to widgets

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
@@ -13,9 +14,8 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Widgets />
       </div>
-
-      {/* Widgets */}
     </div>
   );
 }
