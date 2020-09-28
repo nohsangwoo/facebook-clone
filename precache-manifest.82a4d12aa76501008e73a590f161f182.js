5384d1d303567e02770f9cd9cf20cfedc2eb1dd1
nohsangwoo <nsgr12@naver.com>
2020-09-28 20:06:10 +0900

    Updates

---
+++ precache-manifest.82a4d12aa76501008e73a590f161f182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1581e8984e980f037b1075227c16d85f",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "c892e42a9c5bc1b3ca7d",
+    "url": "/facebook-clone/static/css/main.041d22b0.chunk.css"
+  },
+  {
+    "revision": "737ca917f9c4b18bb144",
+    "url": "/facebook-clone/static/js/2.9094f882.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-clone/static/js/2.9094f882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c892e42a9c5bc1b3ca7d",
+    "url": "/facebook-clone/static/js/main.835e1df4.chunk.js"
+  },
+  {
+    "revision": "33761bbefdcaf2fb5a11",
+    "url": "/facebook-clone/static/js/runtime-main.7a86f86c.js"
+  }
+]);
